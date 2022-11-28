ba7d15e1439f23831efbc8912ee643059006fcc4
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2022-11-28 19:22:18 +0530

    Add Calendar

---
+++ components/PageContainer.js
@@ -0,0 +1,19 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import colors from "../constants/colors";
+
+const PageContainer = (props) => {
+    return (
+        <SafeAreaView style={{ ...styles.container, ...props.style }}>
+            {props.children}
+        </SafeAreaView>
+    );
+};
+
+export default PageContainer;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,78 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useMemo, useState } from "react";
+import PageContainer from "../components/PageContainer";
 import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-} from "react-native";
-import React from "react";
-
+    Agenda,
+    Calendar,
+    CalendarUtils,
+    ExpandableCalendar,
+} from "react-native-calendars";
+let today = new Date().toISOString().slice(0, 10);
+const INITIAL_DATE = today;
 const HomeScreen = () => {
+    const [selected, setSelected] = useState(INITIAL_DATE);
+
+    const onDayPress = useCallback((day) => {
+        setSelected(day.dateString);
+    }, []);
+
+    const getDate = (count) => {
+        const date = new Date(INITIAL_DATE);
+        const newDate = date.setDate(date.getDate() + count);
+        return CalendarUtils.getCalendarDateString(newDate);
+    };
+
+    const marked = useMemo(() => {
+        return {
+            [getDate(0)]: {
+                dotColor: "red",
+                marked: true,
+            },
+            [selected]: {
+                selected: true,
+                disableTouchEvent: true,
+                selectedColor: "orange",
+                selectedTextColor: "red",
+            },
+        };
+    }, [selected]);
     return (
-        <View style={styles.container}>
-            <SafeAreaView>
-                <Text>Home Screen</Text>
-            </SafeAreaView>
-        </View>
+        <PageContainer>
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.welcomeText}>Hi, Lokesh</Text>
+                    <Text style={styles.missText}>Don't miss any events!</Text>
+                </View>
+                <View>
+                    <Calendar
+                        testID={"first_calendar"}
+                        enableSwipeMonths
+                        current={INITIAL_DATE}
+                        style={styles.calendar}
+                        onDayPress={onDayPress}
+                        markedDates={marked}
+                        onDayLongPress={(date) => console.log(date)}
+                    />
+                </View>
+                <View></View>
+            </View>
+        </PageContainer>
     );
 };
+
 export default HomeScreen;
+
 const styles = StyleSheet.create({
+    calendar: {
+        marginBottom: 10,
+    },
     container: {
-        flex: 1,
+        padding: 20,
+    },
+    welcomeText: {
+        fontSize: 24,
+    },
+    missText: {
+        fontSize: 40,
     },
 });
